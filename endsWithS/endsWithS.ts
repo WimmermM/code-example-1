ec16daa21aa5414db77bb16e9d4755f63b6ca6ad
Márk Kővári <mark.kovari@greenfoxacademy.com>
2018-08-16 15:23:09 +0200

    add(endsWithS): base.txt and ts task

---
+++ endsWithS/endsWithS.ts
@@ -0,0 +1,19 @@
+/**
+ * Create and test a method called `endsWithS` that takes a filename as a string
+ *  and retruns the number of the words, that ends with 's'.
+ *  - use only basic control flow statements, like 'for', 'if', etc.
+ *  - do not use built-in methods like 'filter', 'forEach', 'map', etc.
+ *  - write test for 2 different scenarios (test file: getAntiDiagonalAvg.spec.ts)
+ *
+ * Example cases:
+ * 
+ * the correct output for the 'base.txt' is: 26
+ */
+
+'use strict';
+
+
+/********************************
+ * Do not modify the code below *
+ ********************************/
+export default endsWithS;
