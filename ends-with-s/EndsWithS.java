8088ff40f8829d84ff400a7741ca7651b26e04b7
Levente <levente.kobor94@gmail.com>
2018-08-16 15:27:01 +0200

    add(EndsWithS): description

---
+++ ends-with-s/EndsWithS.java
@@ -0,0 +1,13 @@
+public class EndsWithS {
+
+   /**
+   * Create and test a method called `endsWithS` that takes a filename as a string
+   *  and retruns the number of the words, that ends with 's'.
+   *  - use only basic control flow statements, like 'for', 'if', etc.
+   *  - do not use built-in methods like 'filter', 'forEach', 'map', etc.
+   *
+   * Example cases:
+   * 
+   * the correct output for the 'base.txt' is: 26
+   */
+}
