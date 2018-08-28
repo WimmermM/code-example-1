6f58c5d58ca4a283ebe2c54b26a3f25405cb63de
Levente <levente.kobor94@gmail.com>
2018-08-28 14:38:29 +0200

    hotfix

--- ends-with-s/endsWithS.ts
+++ ends-with-s/endsWithS.ts
@@ -1,8 +1,6 @@
 /**
  * Create and test a method called `endsWithS` that takes a filename as a string
  *  and returns the number of the words, that ends with 's'.
- *  - use only basic control flow statements, like 'for', 'if', etc.
- *  - do not use built-in methods like 'filter', 'forEach', 'map', etc.
  * 
  * Example cases:
  * 
